0c070ff1825b93b8bbe185c8eb721c3f6388598f
Saifullah Bukhari <bukhari.saifullah8@gmail.com>
2023-09-22 20:23:35 +0300

    Made the selectionSort method

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
 public class Sorter {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double [] list = {5,6,2,1,3,7};
+
+        selectionSort(list);
+        System.out.println(Arrays.toString(list));
+    }
+
+    public static void selectionSort(double[] list){
+        for(int i = 0; i < list.length - 1; i++){
+            double currentMin = list[i];
+            int currentMinIndex = i;
+            
+            for(int j=i + 1; j < list.length; j++){
+                if(currentMin > list[j]){
+                currentMin = list[j];
+                currentMinIndex = j;
+                }
+            }
+            
+            if(currentMinIndex != i){
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+    
+        }
+
     }
 }
